6c408cfa7b46051eb68a1eac226b5c0f47c7618a
Fırat Alçın <firatalcin04@gmail.com>
2023-12-06 23:58:31 +0300

    Generic Repository Pattern ve Dependency Injection

---
+++ Blogger/Blogger.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Blogger.Data.Repositories.Abstract;
+using Blogger.Data.Repositories.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Data.Extensions
+{
+	public static class DataLayerExtensions
+	{
+		public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			return services;
+		}
+	}
+}
--- Blogger/Blogger.Web/Program.cs
+++ Blogger/Blogger.Web/Program.cs
@@ -1,14 +1,17 @@
 using Blogger.Data.Context;
+using Blogger.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
 	opt.UseSqlServer(builder.Configuration.GetConnectionString("mssql"));
 });
+
 
 var app = builder.Build();
 
